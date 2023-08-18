3f5dad22971898810529beb33a9f8a07861021c6
akmatkulov <akmatkulovs991@gmail.com>
2023-08-18 21:49:49 +0600

    Startef 6 kyu: likes.rb

---
+++ six_kyu/likes.rb
@@ -0,0 +1,13 @@
+def likes(names)
+  if names.length == 1
+    "#{names[0]} likes this"
+  elsif names.length == 2
+    "#{names[0]} and #{names[1]} like this"
+  elsif names.length == 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  elsif names.length == 0
+    "no one likes this"
+  else
+    "#{names[0]}, #{names[1]} and #{names.length - 2} others like this"
+  end
+end
